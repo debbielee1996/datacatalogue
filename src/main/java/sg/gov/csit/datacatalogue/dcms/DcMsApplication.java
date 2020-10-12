e88119521716ba8062458bd579388065332986c1
debbielee1996 <62943444+debbielee1996@users.noreply.github.com>
2020-10-12 18:00:34 +0800

    add settings in main.java to expose dc-ms as API to front end

--- src/main/java/sg/gov/csit/datacatalogue/dcms/DcMsApplication.java
+++ src/main/java/sg/gov/csit/datacatalogue/dcms/DcMsApplication.java
@@ -3,6 +3,9 @@ package sg.gov.csit.datacatalogue.dcms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sg.gov.csit.datacatalogue.dcms.officer.Officer;
 
 @SpringBootApplication
@@ -10,5 +13,16 @@ public class DcMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DcMsApplication.class, args);
+	}
+
+	//https://stackoverflow.com/questions/42874351/spring-boot-enabling-cors-by-application-properties
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+			}
+		};
 	}
 }
