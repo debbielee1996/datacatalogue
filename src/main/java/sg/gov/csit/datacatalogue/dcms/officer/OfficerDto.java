8e359a261d32fe433cc6520d96533ed8fe53155b
debbielee1996 <62943444+debbielee1996@users.noreply.github.com>
2020-12-30 16:56:55 +0800

    set data owner/data custodian
    - updated createDataset method to receive ownerPf and custodianPf (not custodianPfs yet)
    - checks for existence of owner/custodians when adding dataset

---
+++ src/main/java/sg/gov/csit/datacatalogue/dcms/officer/OfficerDto.java
@@ -0,0 +1,13 @@
+package sg.gov.csit.datacatalogue.dcms.officer;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class OfficerDto {
+    private String pf;
+    private String name;
+}
